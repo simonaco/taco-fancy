91f611d1e31822c87498e3a5fe16c73f1c25ce12
Simona Cotin <simona.cotin@gmail.com>
2018-09-10 21:44:45 +0100

    Add tacos-list module

---
+++ ui/src/app/tacos-list/tacos-list.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TacosListRoutingModule } from './tacos-list-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TacosListRoutingModule
+  ],
+  declarations: []
+})
+export class TacosListModule { }
